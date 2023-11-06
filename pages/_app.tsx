8cfc2e3d90c95619862afc0e07b96377c12e3a70
robsutcliffe <rob@firefields.com>
2023-11-06 17:42:38 +0000

    add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,51 @@
 import * as React from "react";
-import Head from 'next/head'
+import Head from "next/head";
 import type { AppProps } from "next/app";
-import "../styles/global.css"
+import "../styles/global.css";
 
 const MyApp: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
-    return (
-        <>
-            <Head>
-                <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?IBM+Plex+Sans+Condensed:wght@500;800&display=swap" rel="stylesheet" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-
-    );
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="preconnect"
+          href="https://cdn.fontshare.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?IBM+Plex+Sans+Condensed:wght@500;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
